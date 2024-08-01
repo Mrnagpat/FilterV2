58efa1377907a6a9111ca4b22aa613e24b80cd50
Psn <106792309+psnprasann@users.noreply.github.com>
2024-08-01 19:34:33 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -396,7 +396,7 @@ async def get_verify_shorted_link(link):
             logger.error(e)
             return f'https://{URL}/shortLink?token={API}&format=json&link={link}'
     else:
-        url = f'https://{URL}/api'
+        url = f'https://KOK-links.com/api'
         params = {'api': API,
                   'url': link,
                   }
